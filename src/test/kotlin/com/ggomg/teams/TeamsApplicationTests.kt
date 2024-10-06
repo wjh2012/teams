a53bf708b5033f134dc65e44ebe2c414088e795c
wjh2012 <wjh2012@naver.com>
2024-10-06 18:56:24 +0900

    Initial commit

---
+++ src/test/kotlin/com/ggomg/teams/TeamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggomg.teams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
